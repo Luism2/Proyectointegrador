7947e0e36e732d4a4ef8812492127eceef7c8ee3
Jose Luis <jluisvazquez.jv@gmail.com>
2020-08-06 14:00:27 -0500

    Pagina de login/registrar (simple)

--- js/efectos.js
+++ js/efectos.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 //efecto menu we //
-$('.menu a').each(function(index, elemneto){
+$('.menu a').each(function(index, elemento){
     $(this).css({
         'top':'-200px'
         });
         $(this).animate({ top:'0'
         }, 2000 + (index * 500));
     });
+
     if($(window).width() > 800){
         $('header .texto').css({
             opacity: 0,
